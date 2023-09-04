136ad47b57c5c5b99cf83f226c6b0a67b52d4668
Rui Lun Tran <ruilun.tran@gmail.com>
2023-09-04 15:47:09 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import {parse} from 'papaparse';
 import {findBestMatch} from 'string-similarity';
 
-function knxDatapointTypeMapper(dataPoint) {
-  const partial = dataPoint.split('-');
+export function knxDatapointTypeMapper(dataPoint: string): string {
+  const partial: Array<string | number> = dataPoint.split('-');
 
   if (partial.length === 2) {
     partial[2] = 1;
@@ -11,7 +11,7 @@ function knxDatapointTypeMapper(dataPoint) {
   return 'DPT' + partial[1] + '.' + ('000' + partial[2]).substr(-3);
 }
 
-function parseETSGroupAddressCSVExport(csv, rooms) {
+export function parseETSGroupAddressCSVExport(csv: any, rooms: Array<string>): object {
 
   if (!Array.isArray(rooms)) {
     throw Error('An array is expected for rooms. E.g. ["livingRoom"]');
@@ -24,10 +24,10 @@ function parseETSGroupAddressCSVExport(csv, rooms) {
     }).data;
 
     return parsedData
-      .filter(item => item.DatapointType !== '')
-      .filter(item => typeof item.DatapointType !== 'undefined')
+      .filter(item => (item as any).DatapointType !== '')
+      .filter(item => typeof (item as any).DatapointType !== 'undefined')
       .map(item => {
-        const _item = item;
+        const _item: any = item;
 
         let room;
         if (_item.Sub) {
@@ -38,17 +38,11 @@ function parseETSGroupAddressCSVExport(csv, rooms) {
         return {
           room: room ? room : 'unknown',
           address: _item.Address,
-          datapointType: knxDatapointTypeMapper.knxDatapointTypeMapper(_item.DatapointType)
+          datapointType: this.knxDatapointTypeMapper(_item.DatapointType)
         }
       })
   } catch (e) {
     throw Error(e);
   }
 
-
-}
-
-export {
-  knxDatapointTypeMapper,
-  parseETSGroupAddressCSVExport
 }
